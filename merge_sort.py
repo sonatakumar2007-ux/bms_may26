0c11ded89dacd860857c673f5e18a8d247c9aa18
sonatakumar2007-ux <sonatakumar2007@gmail.com>
2026-05-13 18:31:54 +0530

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Find the middle point and split the array
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursive call on each half
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Iterators for traversing the two halves and the main array
+        i = j = k = 0
+
+        # Copy data to temp arrays left_half[] and right_half[]
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left in left_half
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        # Checking if any element was left in right_half
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
